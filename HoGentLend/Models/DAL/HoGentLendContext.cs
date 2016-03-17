87b67f32bf8474c3f7a47420b4a1d2c5731f416d
WhatsInTheBoks <sven_dedeene@hotmail.com>
2016-03-17 11:19:19 +0100

    Project Setup (Alle packages, DbContext en Owin Login systeem)

---
+++ HoGentLend/Models/DAL/HoGentLendContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoGentLend.Models.DAL
+{
+    public class HoGentLendDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public HoGentLendDbContext() : base("HoGentLend")
+        { }
+
+        // hier komen de DbSet's
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public static HoGentLendDbContext Create()
+        {
+            return DependencyResolver.Current.GetService<HoGentLendDbContext>();
+        }
+
+        static HoGentLendDbContext()
+        {
+            Console.WriteLine("setting the initializer");
+            Database.SetInitializer<HoGentLendDbContext>(new HoGentLendDbInitializer());
+        }
+        public static void init() { Create().Database.Initialize(true); }
+    }
+
+}
---
+++ HoGentLend/Models/DAL/HoGentLendDbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+
+namespace HoGentLend.Models.DAL
+{
+    public class HoGentLendDbInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<HoGentLendDbContext>
+    {
+        protected override void Seed(HoGentLendDbContext context)
+        {
+            try
+            {
+                // Hier zetten we database context initializatie
+
+                context.SaveChanges();
+                base.Seed(context);
+            }
+            catch (DbEntityValidationException e)
+            {
+                string s = "Fout creatie database ";
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    s += String.Format("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.GetValidationResult());
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        s += String.Format("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw new Exception(s);
+            }
+        }
+    }
+}
