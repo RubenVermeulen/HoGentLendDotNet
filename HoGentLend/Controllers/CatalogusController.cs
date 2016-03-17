ef78b9d3e2d4b6d63396a63c4925984441af0030
WhatsInTheBoks <sven_dedeene@hotmail.com>
2016-03-17 14:17:57 +0100

    Sprint 1: Controller Class Diagram

---
+++ HoGentLend/Controllers/CatalogusController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoGentLend.Controllers
+{
+    public class CatalogusController : Controller
+    {
+        private IMateriaalRepository _materiaalRepository;
+
+        public IMateriaalRepository MateriaalRepository
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        // GET: Catalogus
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        public ActionResult Filter()
+        {
+            return View("Index");
+        }
+    }
+}
