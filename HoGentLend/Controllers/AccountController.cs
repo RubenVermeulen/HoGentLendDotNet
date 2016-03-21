c221a78c46e7b74993f82d788414b74099ce5135
WhatsInTheBoks <sven_dedeene@hotmail.com>
2016-03-21 19:15:47 +0100

    AccountController (LoginAction, LoginView)

--- HoGentLend/Controllers/AccountController.cs
+++ HoGentLend/Controllers/AccountController.cs
@@ -56,6 +56,11 @@ namespace HoGentLend.Controllers
             }
         }
 
+        public AccountController(IGebruikerRepository gebruikerRepo)
+        {
+            this.gebruikerRepo = gebruikerRepo;
+        }
+
         //
         // GET: /Account/Login
         [AllowAnonymous]
@@ -117,7 +122,11 @@ namespace HoGentLend.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+
                     
+
+
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
