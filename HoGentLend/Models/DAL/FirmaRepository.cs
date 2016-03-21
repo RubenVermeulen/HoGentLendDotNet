87a3a30e0e5bd1b4b4eba02958f436be29fd5564
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-03-21 20:58:32 +0100

    Repositories.

---
+++ HoGentLend/Models/DAL/FirmaRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using HoGentLend.Models.Domain;
+
+namespace HoGentLend.Models.DAL
+{
+    public class FirmaRepository : IFirmaRepository
+    {
+        private HoGentLendContext ctx;
+        private DbSet<Firma> firmas;
+
+        public FirmaRepository(HoGentLendContext ctx)
+        {
+            this.ctx = ctx;
+            this.firmas = ctx.Firmas;
+        } 
+
+        public Firma FindBy(string id)
+        {
+            return firmas.Find(id);
+        }
+
+        public IQueryable<Firma> FindAll()
+        {
+            return firmas;
+        }
+
+        public void Add(Firma entity)
+        {
+            firmas.Add(entity);
+        }
+
+        public void Delete(Firma entity)
+        {
+            firmas.Remove(entity);
+        }
+
+        public void SaveChanges()
+        {
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ HoGentLend/Models/DAL/MateriaalRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using HoGentLend.Models.Domain;
+
+namespace HoGentLend.Models.DAL
+{
+    public class MateriaalRepository : IMateriaalRepository
+    {
+        private HoGentLendContext ctx;
+        private DbSet<Materiaal> materialen;
+
+        public MateriaalRepository(HoGentLendContext ctx)
+        {
+            this.ctx = ctx;
+            this.materialen = ctx.Materialen;
+        }
+         
+        public Materiaal FindBy(string id)
+        {
+            return materialen.Find(id);
+        }
+
+        public IQueryable<Materiaal> FindAll()
+        {
+            return materialen;
+        }
+
+        public void Add(Materiaal entity)
+        {
+            materialen.Add(entity);
+        }
+
+        public void Delete(Materiaal entity)
+        {
+            materialen.Remove(entity);
+        }
+
+        public void SaveChanges()
+        {
+            ctx.SaveChanges();
+        }
+    }
+}
