e208e62283dee48da831d724383b51ab068ca589
Alexander Van Damme <alexanderdebest@live.be>
2016-04-13 15:58:21 +0200

    WishList ipv wishList schrijffoutje

--- HoGentLend/Models/Domain/Gebruiker.cs
+++ HoGentLend/Models/Domain/Gebruiker.cs
@@ -15,8 +15,9 @@ namespace HoGentLend.Models.Domain
         public string Email { get; set; }
         public bool IsLector { get; set; }
 
-        public VerlangLijst WishList { get; set; }
+        public VerlangLijst WishList;
 
+       
 
         public bool DoShowAllMaterials()
         {
