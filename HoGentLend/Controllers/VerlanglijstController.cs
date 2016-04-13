f106fc2b1aea2369a37be0456a72b019fd6ccd4f
Ruben Vermeulen <ruben.vermeulen@hotail.com>
2016-04-13 11:05:53 +0200

    VerlanglijstController aangemaakt

---
+++ HoGentLend/Controllers/VerlanglijstController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HoGentLend.Models.Domain;
+
+namespace HoGentLend.Controllers
+{
+    public class VerlanglijstController : Controller
+    {
+        // GET: Verlanglijst
+        public ActionResult Index(Gebruiker gebruiker)
+        {
+            return View();
+        }
+    }
+}
