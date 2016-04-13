62eb10a7734902d15d24f57491adb07d92ebe9ea
WhatsInTheBoks <sven_dedeene@hotmail.com>
2016-04-13 23:11:27 +0200

    AccountController aangepast (geen wachtwoorden meer opslaan en offline provider tijdens development)

---
+++ HoGentLend/Models/Domain/HoGentApi/OfflineHoGentApiLookupProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoGentLend.Models.Domain.HoGentApi
+{
+    public class OfflineHoGentApiLookupProvider : IHoGentApiLookupProvider
+    {
+        public HoGentApiLookupResult Lookup(string userId, string unhashedPassword)
+        {
+            if (userId == "offtest" && unhashedPassword == "offtest")
+            {
+                return new HoGentApiLookupResult()
+                {
+                    Faculteit = "FBO",
+                    FirstName = "Offline",
+                    LastName  = "Student",
+                    Email = "offline.student@hogent.be",
+                    Type = "student"
+                };
+            }
+            return new HoGentApiLookupResult();
+        }
+    }
+}
