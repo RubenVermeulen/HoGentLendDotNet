b958623154dc7307149fed3e6b4dc23045d11083
Xander Berkein <xander.berkein.v7629@student.hogent.be>
2016-04-17 14:57:40 +0200

    Domein Reservatie

--- HoGentLend/Models/Domain/DAL/ReservatieRepository.cs
+++ HoGentLend/Models/Domain/DAL/ReservatieRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace HoGentLend.Models.Domain
+namespace HoGentLend.Models.Domain.DAL
 {
     public class ReservatieRepository
     {
