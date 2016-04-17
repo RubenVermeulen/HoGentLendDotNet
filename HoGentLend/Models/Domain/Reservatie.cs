21955dbf2f13032d3d99b6b346479ea0f6e72bab
Xander Berkein <xander.berkein.v7629@student.hogent.be>
2016-04-17 14:55:18 +0200

    Domain Reservatie - Reservatie, ReservatieLijn en Repo toegevoegd

---
+++ HoGentLend/Models/Domain/Reservatie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoGentLend.Models.Domain
+{
+    public class Reservatie
+    {
+
+        public long Id { get; private set; }
+
+        public virtual Gebruiker Lener { get; set; }
+        public DateTime Ophaalmoment { get; set; }
+        public DateTime Indienmoment { get; set; }
+        public DateTime Reservatiemoment { get; set; }
+        public bool Opgehaald { get; set; }
+        public virtual List<ReservatieLijn> ReservatieLijnen  { get; set; }
+
+
+    }
+}
