9270ee9f037532bf6d6c564033a6c89b33932524
WhatsInTheBoks <sven_dedeene@hotmail.com>
2016-04-20 19:53:20 +0200

    get pranked

--- HoGentLend/Models/Domain/DAL/HoGentLendDbInitializer.cs
+++ HoGentLend/Models/Domain/DAL/HoGentLendDbInitializer.cs
@@ -141,14 +141,14 @@ namespace HoGentLend.Models.DAL
                 DateTime _28April2016 = new DateTime(2016, 4, 28);
 
                 Reservatie r1 = new Reservatie(g1, _13April2016, _20April2016);
-                r1.ReservatieLijnen.Add(new ReservatieLijn(2, _13April2016, _20April2016, m1);
-                r1.ReservatieLijnen.Add(new ReservatieLijn(3, _13April2016, _20April2016, m2);
-                r1.ReservatieLijnen.Add(new ReservatieLijn(4, _13April2016, _20April2016, m3);
+                r1.ReservatieLijnen.Add(new ReservatieLijn(2, _13April2016, _20April2016, m1));
+                r1.ReservatieLijnen.Add(new ReservatieLijn(3, _13April2016, _20April2016, m2));
+                r1.ReservatieLijnen.Add(new ReservatieLijn(4, _13April2016, _20April2016, m3));
 
                 Reservatie r2 = new Reservatie(g2, _21April2016, _28April2016);
-                r2.ReservatieLijnen.Add(new ReservatieLijn(2, _13April2016, _20April2016, m4);
-                r2.ReservatieLijnen.Add(new ReservatieLijn(3, _13April2016, _20April2016, m5);
-                r2.ReservatieLijnen.Add(new ReservatieLijn(4, _13April2016, _20April2016, m3);
+                r2.ReservatieLijnen.Add(new ReservatieLijn(2, _13April2016, _20April2016, m4));
+                r2.ReservatieLijnen.Add(new ReservatieLijn(3, _13April2016, _20April2016, m5));
+                r2.ReservatieLijnen.Add(new ReservatieLijn(4, _13April2016, _20April2016, m3));
 
                 context.Reservaties.Add(r1);
                 context.Reservaties.Add(r2);
